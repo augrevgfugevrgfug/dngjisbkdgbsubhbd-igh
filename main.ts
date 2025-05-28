b8624fd46ace6a4d95eddc08586a1888e6199de6
augrevgfugevrgfug <as0513@stu.ttct.edu.tw>
2025-05-28 11:55:48 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,19 @@
+let a = 10
+basic.showNumber(a / 2)
+basic.pause(1000)
+let b = 2
+basic.showNumber(b * 2)
+basic.pause(1000)
+let c = 3
+basic.showNumber(c * 1)
+basic.pause(1000)
+let d = 2
+basic.showNumber(d * 1)
+basic.pause(1000)
+let e = 2
+basic.showNumber(e / 2)
+basic.pause(1000)
+let f = 0
+basic.showNumber(f * 1)
+basic.pause(1000)
+basic.showString("Game starts")
